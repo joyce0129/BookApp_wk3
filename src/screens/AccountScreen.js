80e60c5a6f2f8681b286883d822e529e1a818780
joyce0129 <131430612+joyce0129@users.noreply.github.com>
2024-03-23 12:33:53 +0800

    Update AccountScreen.js

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -7,13 +7,6 @@ import { MyDrawer } from "../navigation";
 const AccountScreen =(navigation)=>{
     return(
         <View style={{flex:1}}>
-        {/* //     <Pressable onPress={() => navigation.goBack()}>
-        //   <Image */}
-        {/* //     source = {{uri: 'https://github.com/joyce0129/BookApp_wk3/blob/main/img/direction.png?raw=true'}}
-        //     style={{width: 25, height: 25}}
-        //   />
-        // </Pressable> */}
-        
             <Text fontSize={30}>
             This is a AccountScreen Page
             </Text>
